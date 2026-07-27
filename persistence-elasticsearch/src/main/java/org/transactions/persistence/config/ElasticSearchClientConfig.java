08866ab5776f7da7f87e28aa366e74ad02351db1
Julien Girard <girard.julien@gmail.com>
2026-07-27 14:29:56 +0200

    feat(pg) : Fix elasticsearch compatibility

--- persistence-elasticsearch/src/main/java/org/transactions/persistence/config/ElasticSearchClientConfig.java
+++ persistence-elasticsearch/src/main/java/org/transactions/persistence/config/ElasticSearchClientConfig.java
@@ -4,6 +4,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.elasticsearch.client.ClientConfiguration;
 import org.springframework.data.elasticsearch.client.elc.ElasticsearchConfiguration;
+import org.springframework.data.elasticsearch.support.HttpHeaders;
+
 
 @Configuration
 public class ElasticSearchClientConfig extends ElasticsearchConfiguration {
@@ -13,8 +15,14 @@ public class ElasticSearchClientConfig extends ElasticsearchConfiguration {
 
     @Override
     public ClientConfiguration clientConfiguration() {
+
+        HttpHeaders defaultHeaders = new HttpHeaders();
+        defaultHeaders.add("Accept", "application/vnd.elasticsearch+json;compatible-with=8");
+        defaultHeaders.add("Content-Type", "application/vnd.elasticsearch+json;compatible-with=8");
+
         return ClientConfiguration.builder()
                 .connectedTo(esConfig.getHostname())
+                .withDefaultHeaders(defaultHeaders)
                 .build();
     }
 }
